82d67b1ad861525b3f1389065705a882bbbb6f04
Pandu Galang <pandu4431@gmail.com>
2019-03-24 04:25:38 +0700

    NEXT

--- src/main/java/com/example/overtime/controller/UltimateController.java
+++ src/main/java/com/example/overtime/controller/UltimateController.java
@@ -5,7 +5,13 @@
  */
 package com.example.overtime.controller;
 
+import com.example.overtime.serviceimpl.DivisionDAO;
 import com.example.overtime.serviceimpl.EmailService;
+import com.example.overtime.serviceimpl.EmployeeDAO;
+import com.example.overtime.serviceimpl.JobDAO;
+import com.example.overtime.serviceimpl.OvertimeDAO;
+import com.example.overtime.serviceimpl.SiteDAO;
+import com.example.overtime.serviceimpl.TimeSheetDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +29,23 @@ public class UltimateController {
     private static Logger log = LoggerFactory.getLogger(UltimateController.class);
 
     @Autowired
-    RegionDAO rdao;
+    DivisionDAO ddao;
 
-    
+    @Autowired
+    EmployeeDAO edao;
+
+    @Autowired
+    JobDAO jdao;
+
+    @Autowired
+    OvertimeDAO odao;
+
+    @Autowired
+    SiteDAO sdao;
+
+    @Autowired
+    TimeSheetDAO tdao;
+
     @Autowired
     private EmailService emailService;
 
@@ -47,7 +67,23 @@ public class UltimateController {
         }
         return "redirect:/";
     }
-    
-    
+
+//    REMAPING ALL THE CONTROLLER NEED
+//    CONTROLLER LIST
+//    GET
+//    -GET ALL DATA DIVISION 
+//    -GET ALL DATA EMPLOYEE 
+//    -GET ALL DATA OVERTIME 
+//    -GET ALL DATA SITE 
+//    -GET ALL DATA JOB 
+//    -GET ALL DATA TIMESHEET 
+//            
+//    POST
+//    -POST DATA
+//    -OVERTIME(REQUEST OVERTIME)
+//    -POST DATA EMPLOYE(ADMIN)
+                                    
+            
+            
 
 }
